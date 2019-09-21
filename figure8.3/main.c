1f773ea9aa52351839f263d0d3abfe7a05984b35
book <book@book-desktop.(none)>
2019-09-21 10:04:04 +0800

    vfork and fork

---
+++ figure8.3/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int globvar = 6; /* external variable in initialized data */
+int main(void)
+{
+	int var; /* automatic variable on the stack */
+	pid_t pid;
+	var = 88;
+	printf("before vfork\n"); /* we don't flush stdio */
+	if ((pid = vfork()) < 0) {
+		printf("vfork error\n");
+	} else if (pid == 0) { /* child */
+		globvar++; /* modify parents variables */
+		var++;
+		exit(0); /* child terminates */
+	}
+	/* parent continues here */
+	printf("pid = %ld, glob = %d, var = %d\n", (long)getpid(), globvar,var);
+	exit(0);
+}
